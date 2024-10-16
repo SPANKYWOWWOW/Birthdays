f4aadae2d63e96b0a0b03c2e0cee9e979c60d2d5
SPANKYWOWWOW <pavandavar99@gmail.com>
2024-10-16 11:19:52 -0400

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Pavan Davar on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
